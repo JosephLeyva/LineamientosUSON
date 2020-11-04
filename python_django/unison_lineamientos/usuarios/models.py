07fdf1942db6caa2889f6b01f677e3bfcaca7a7e
José Luis Aguilera Luzania <43888983+JoseLuis-AL@users.noreply.github.com>
2020-11-03 17:22:54 -0700

    JLAL - Usuarios Database
    Se añadio la tabla de usuarios a la database de django según los atributos del usuario.

--- python_django/unison_lineamientos/usuarios/models.py
+++ python_django/unison_lineamientos/usuarios/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Usuario(models.Model):
+
+    ROLES = (
+        ('Comisión', 'comision'),
+        ('Investigador', 'investigador'),
+        ('Estudiante investigador', 'estudiante_investigador')
+    )
+
+    nombre = models.CharField(max_length=50, null=True)
+    email = models.CharField(max_length=50, null=True)
+    password = models.CharField(max_length=50, null=True)
+    rol = models.CharField(max_length=50, null=True, choices=ROLES)
+
+    def __str__(self):
+        return self.nombre
