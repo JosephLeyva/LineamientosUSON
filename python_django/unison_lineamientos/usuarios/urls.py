6fb36e6906e9dc80ccd0b56bb61f2e272ebbb2c8
José Luis Aguilera Luzania <43888983+JoseLuis-AL@users.noreply.github.com>
2020-11-03 18:52:10 -0700

    JLAL - Funcionalidad y data
    Se añadieron las siguientes funcionalidades a las páginas:
    
    1. La barra de navegación ahora redirecciona a la página correspondiente.
    
    2. La página de registro ahora cuenta con un menú para seleccionar el rol del usuario.
    
    3. Se añadio la página de usuarios con fines unicamente de pruebas. La página usuarios no estará en la versión final.

--- python_django/unison_lineamientos/usuarios/urls.py
+++ python_django/unison_lineamientos/usuarios/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('registro/', views.registro)
+    path('', views.home, name='home'),
+    path('registro/', views.registro, name='registro'),
+    path('usuarios/', views.usuarios_data, name='usuarios')
 ]
--- python_django/unison_lineamientos/usuarios/views.py
+++ python_django/unison_lineamientos/usuarios/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 
 # Create your views here.
@@ -10,3 +11,12 @@ def home(request):
 
 def registro(request):
     return render(request, 'usuarios/registro.html')
+
+
+def usuarios_data(request):
+    users = Usuario.objects.all()
+
+    context = {'users': users}
+
+    return render(request, 'usuarios/usuarios_data.html', context)
+
