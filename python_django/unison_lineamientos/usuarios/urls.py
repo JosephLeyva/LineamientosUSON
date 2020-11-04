ae37f85df89541c9d5445690b4d8fd1af92b80af
José Luis Aguilera Luzania <43888983+JoseLuis-AL@users.noreply.github.com>
2020-11-03 21:29:49 -0700

    JLAL- Se añadio un registro de usuarios y un login

--- python_django/unison_lineamientos/usuarios/urls.py
+++ python_django/unison_lineamientos/usuarios/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('registro/', views.registro, name='registro'),
+    path('inicio_sesion/', views.inicio_sesion, name='inicio_sesion'),
     path('usuarios/', views.usuarios_data, name='usuarios')
 ]
--- python_django/unison_lineamientos/usuarios/views.py
+++ python_django/unison_lineamientos/usuarios/views.py
@@ -1,16 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 # Create your views here.
+from .models import *
+from .forms import CreateUserForm
+
 
 def home(request):
     return render(request, 'usuarios/home.html')
 
 
 def registro(request):
-    return render(request, 'usuarios/registro.html')
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            nombre_nuevo_usuario = form.cleaned_data.get('username')
+            messages.success(request, 'Usuario registrado como: ' + nombre_nuevo_usuario)
+            return redirect('inicio_sesion')
+
+    context = {'form': form}
+    return render(request, 'usuarios/registro.html', context)
+
+
+def inicio_sesion(request):
+
+    if request == 'POST':
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+    context = {}
+    return render(request, 'usuarios/inicio_sesion.html', context)
 
 
 def usuarios_data(request):
